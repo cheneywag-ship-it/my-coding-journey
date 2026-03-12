0472c01fcd40c02fe347c19f5dcd2c485f34f29d
chenery <wangchenery@gmail.com>
2026-03-12 16:13:43 +0800

    新增c语言第一步动作向世界打招呼

---
+++ 01-c-language/basics/01-helloword.c
@@ -0,0 +1,12 @@
+// 引入标准输入输出库头文件，用于支持printf等I/O函数
+#include <stdio.h>
+
+// 程序唯一入口点，操作系统调用此函数启动执行
+int main()
+{
+    // 输出字符串到控制台，\n表示换行符
+    printf("Hello from macOS!\n");
+
+    // 返回0表示程序成功终止
+    return 0;
+}
